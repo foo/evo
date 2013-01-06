652eaedecde72b5b75437f635b68c07224647707
Michał Karpiński <karp0506@gmail.com>
2013-01-06 19:39:06 +0100

    changed hours to minutes

--- src/sdvrp.cpp
+++ src/sdvrp.cpp
@@ -12,29 +12,31 @@ void sdvrp::initialize(int N)
 int sdvrp::route_cost(const std::vector<int> &v)
 {
   int days = 0;
-  int hours_used = 0;
+  int minutes_used = 0;
   for(unsigned int i=0; i<size-1; i+=3)
   {
     const int v1 = v[i];
     const int v2 = v[i+1];
     const int v3 = v[i+2];
     const int base = size - 1;
+    const int hour = 60;
+    const int business_day = 8*hour;
 
     // cost trip in hours
     const int trip_cost = graph[base][v1] + graph[v1][v2] + graph[v2][v3] + graph[v3][base]; 
-    if(trip_cost > 8)
+    if(trip_cost > business_day)
       return INT_MAX; // infinity
     
-    if(trip_cost + hours_used <= 8)
-      hours_used += trip_cost;
+    if(trip_cost + minutes_used <= business_day)
+      minutes_used += trip_cost;
     else
       {
 	// new day job
-	hours_used = 0;
+	minutes_used = 0;
 	++days;
       }
   }
-  if(hours_used != 0)
+  if(minutes_used != 0)
     ++days;
   return days;
 }
